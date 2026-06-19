6ed7af77996dd765d7a988a0661990bfd6be6e10
Kaio Amim <144812141+KaioAmim@users.noreply.github.com>
2026-06-19 10:45:42 -0300

    modulo-05

---
+++ modulo-05/promisses/promises.js
@@ -0,0 +1,23 @@
+function testes(){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            const erro = false;
+
+            if(erro){
+                reject('Erro...');
+            }else{
+                resolve("A promisse foi resolvida com sucesso!");
+            }
+        }, 1000);
+        
+    })
+}
+
+async function testes2(){
+    await testes().then(function(res){
+        alert(res);
+    });
+    alert("olá");
+}
+
+testes2();
